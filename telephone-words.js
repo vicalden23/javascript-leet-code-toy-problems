ad086ae4ef28b76c2e59d8d1480298a75d36482d
vic.alden23 <vic.alden23@gmail.com>
2018-07-15 12:13:22 -0700

    solves telephone words

---
+++ telephone-words.js
@@ -0,0 +1,64 @@
+/*
+  * Each number key on a standard phone keypad has a set of Latin letters written on
+  * it as well: http://en.wikipedia.org/wiki/File:Telephone-keypad2.svg
+  *
+  * Businesses often try to come up with clever ways to spell out their phone number
+  * in advertisements to make it more memorable. But there are a lot of combinations!
+  *
+  * Write a function that takes up to four digits of a phone number, and
+  * returns a list of all of the words that can be written on the phone with
+  * that number. (You should return all permutations, not only English words.)
+  *
+  * Example:
+  *   telephoneWords('2745');
+  *   => ['APGJ',
+  *        'APGK',
+  *        'APGL',
+  *        ..., // many many more of these
+  *        'CSIL']
+  *
+  * Tips:
+  *   - Phone numbers are strings! (A phone number can start with a zero.)
+  *   - The digits 0 and 1 do not have letters associated with them, so they should be left as numbers.
+  *   - Don't return every combination of those digits in any order, just the order given.
+  *
+  *  Extra credit: There's a list of English dictionary words at /usr/share/dict/words .
+  *  Why not filter your results to only return words contained in that file?
+  *
+  */
+
+var phoneDigitsToLetters = {
+  0: '0',
+  1: '1',
+  2: 'ABC',
+  3: 'DEF',
+  4: 'GHI',
+  5: 'JKL',
+  6: 'MNO',
+  7: 'PQRS',
+  8: 'TUV',
+  9: 'WXYZ'
+};
+
+//use recursion to generate every possible outcome, then compare the outcomes to a list of words, return the words that match
+
+  // TODO: return every combination that can be spelled on a phone with these digits
+
+let telephoneWords = (digits) => {
+  let possibilities = []
+
+  let findPossibilities = (string, digitIndex) => {
+    if (string.length === digits.length) {
+      possibilities.push(string)
+      return
+
+    }
+    let letters = phoneDigitsToLetters[digits[digitIndex]]
+    for (let i = 0; i < letters.length; i++) {
+      findPossibilities(string + letters[i], digitIndex + 1)
+    }
+  }
+  findPossibilities('', 0)
+  return possibilities
+}
+
